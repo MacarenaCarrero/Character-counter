50ac184baca63140d56eda2503fb4ec7f59c7bfe
MacarenaCarrero <macarena.c.rios@gmail.com>
2025-04-06 19:12:29 +0200

    characterCounter

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,25 @@ const counterSentencesElement = document.getElementById('counterSentences');
 
 // area de texto
 const textAreaElement = document.getElementById('textArea');
+// checkBox
+const checkboxElement = document.getElementById('checkbox');
+
+//funcion para character
+
+const checkCounterCharacter = () => {
+  let text = textAreaElement.value;
+  let textLength = text.length;
+  counterCharactersElement.textContent = textLength;
+};
+
+// funcion para words
+let totalWords = 0;
+const checkCounterWords = () => {
+  if (text.endsWith(' ') || text.endsWith('.')) {
+    totalWords++;
+  }
+  counterWordsElement.textContent = totalWords;
+};
+
+textAreaElement.addEventListener('input', checkCounterCharacter);
+textAreaElement.addEventListener('input', checkCounterWords);
