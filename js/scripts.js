948182673080bb5cd18a537d8034a18dcc3bebf8
MacarenaCarrero <macarena.c.rios@gmail.com>
2025-04-06 16:07:29 +0200

    characterCounter

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,6 @@
 const counterCharactersElement = document.getElementById('counterCharacters');
 const counterWordsElement = document.getElementById('counterWords');
 const counterSentencesElement = document.getElementById('counterSentences');
+
+// area de texto
+const textAreaElement = document.getElementById('textArea');
