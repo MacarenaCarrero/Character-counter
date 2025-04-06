a3b7a432769bf3dd6960c419667523ad6b26c37d
MacarenaCarrero <macarena.c.rios@gmail.com>
2025-04-06 22:16:17 +0200

    characterCounter

--- js/scripts.js
+++ js/scripts.js
@@ -9,28 +9,27 @@ const textAreaElement = document.getElementById('textArea');
 const checkboxElement = document.getElementById('checkbox');
 
 //funcion para analizar el texto
-
+let text = '';
 const checkText = () => {
-  let text = textAreaElement.value;
+  text = textAreaElement.value;
   let word = text.match(/\b\w+\b/g);
 
-  counterCharactersElement.textContent = textLength;
+  counterCharactersElement.textContent = text.length;
   counterWordsElement.textContent = word.length;
 };
 
 const deleteSpace = () => {
-  const noSpace = textAreaElement.replaceAll(' ', '');
   if (checkboxElement.checked) {
-    return noSpace;
+    textreplaceAll(' ', '');
   } else {
-    return text;
+    text;
   }
 };
 
-const checkCountSentences = () => {
-  counterSentencesElement.textContent = sentence.length;
-};
+// const checkCountSentences = () => {
+//   counterSentencesElement.textContent = sentence.length;
+// };
 
 textAreaElement.addEventListener('input', checkText);
-textAreaElement.addEventListener('input', checkCountSentences);
+// textAreaElement.addEventListener('input', checkCountSentences);
 checkboxElement.addEventListener('click', deleteSpace);
